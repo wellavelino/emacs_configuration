ea65972bed0615566ba484eebbc46667da897ab0
Wellington Avelino dos Santos <wellington.avelino@outlook.com>
2021-02-15 18:02:02 +0100

    Update init.el
    remove marmelade as repository and remove typo

--- init.el
+++ init.el
@@ -1,11 +1,10 @@
-3;; package -- Emacs Configuration
+;; package -- Emacs Configuration
 
 (package-initialize)
 
 ;; Repositories
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-			 ("marmalade" . "http://marmalade-repo.org/packages/")
-			 ("melpa" . "http://melpa.milkbox.net/packages/")))
+			 ("melpa" . "http://melpa.org/packages/")))
 
 
 ; list the packages you want
